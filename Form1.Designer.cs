58c91c54b76882d8d83f8bc754f444d253a3b8de
Gevorah <jhonsebastianijajiortiz@gmail.com>
2021-02-20 14:37:50 -0500

    datagrid and data set

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,15 @@
         private void InitializeComponent()
         {
             this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.bt_File = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.data = new System.Windows.Forms.DataGridView();
+            this.categories = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.data)).BeginInit();
             this.SuspendLayout();
             // 
             // gMapControl1
@@ -46,7 +49,7 @@
             this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gMapControl1.LevelsKeepInMemmory = 5;
             this.gMapControl1.Location = new System.Drawing.Point(448, 191);
-            this.gMapControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gMapControl1.Margin = new System.Windows.Forms.Padding(2);
             this.gMapControl1.MarkersEnabled = true;
             this.gMapControl1.MaxZoom = 2;
             this.gMapControl1.MinZoom = 2;
@@ -64,34 +67,32 @@
             this.gMapControl1.TabIndex = 0;
             this.gMapControl1.Zoom = 0D;
             // 
-            // dataGridView1
+            // data
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(57, 191);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(312, 217);
-            this.dataGridView1.TabIndex = 1;
+            this.data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data.Location = new System.Drawing.Point(57, 191);
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(312, 217);
+            this.data.TabIndex = 1;
             // 
-            // bt_File
+            // categories
             // 
-            this.bt_File.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.bt_File.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bt_File.ForeColor = System.Drawing.SystemColors.Control;
-            this.bt_File.Location = new System.Drawing.Point(367, 12);
-            this.bt_File.Name = "bt_File";
-            this.bt_File.Size = new System.Drawing.Size(79, 34);
-            this.bt_File.TabIndex = 2;
-            this.bt_File.Text = "Upload File";
-            this.bt_File.UseVisualStyleBackColor = false;
-            this.bt_File.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(132, 94);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(88, 21);
-            this.comboBox1.TabIndex = 3;
+            this.categories.FormattingEnabled = true;
+            this.categories.Items.AddRange(new object[] {
+            "Name",
+            "ID",
+            "Name Type",
+            "Recclass",
+            "Mass",
+            "Fall",
+            "Year",
+            "Reclat",
+            "Relong",
+            "GeoLocation"});
+            this.categories.Location = new System.Drawing.Point(132, 94);
+            this.categories.Name = "categories";
+            this.categories.Size = new System.Drawing.Size(88, 21);
+            this.categories.TabIndex = 3;
             // 
             // label1
             // 
@@ -102,7 +103,6 @@
             this.label1.Size = new System.Drawing.Size(41, 16);
             this.label1.TabIndex = 4;
             this.label1.Text = "Filter";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
@@ -114,22 +114,54 @@
             this.label2.TabIndex = 5;
             this.label2.Text = "attribute";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(132, 147);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(356, 148);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(591, 147);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(42, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(658, 148);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(42, 20);
+            this.textBox3.TabIndex = 9;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(793, 420);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.bt_File);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.categories);
+            this.Controls.Add(this.data);
             this.Controls.Add(this.gMapControl1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.data)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,11 +170,14 @@
         #endregion
 
         private GMap.NET.WindowsForms.GMapControl gMapControl1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button bt_File;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView data;
+        private System.Windows.Forms.ComboBox categories;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,24 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace meteorite_falls
 {
     public partial class Form1 : Form
     {
+
+        public const string FILE = "data/meteoritos_2000-2010.csv";
+
         public Form1()
         {
             InitializeComponent();
+            init();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
+        private DataTable dt = new DataTable();
 
+        private void init()
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(FILE);
+                string line;
+                bool f = true;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] sl = line.Split(',');
+                    if (f == true)
+                    {
+                        for (int k = 0; k < sl.Length; k++)
+                        {
+                            dt.Columns.Add(sl[k]);
+                        }
+                        f = false;
+                    }
+                    else
+                    {
+                        dt.Rows.Add(sl);
+                    }
+                }
+                data.DataSource = dt;
+            }
+            catch (IOException)
+            {
+
+            }
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void cb_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            dt.DefaultView.RowFilter = string.Format("Convert([{0}], 'System.String') LIKE '{1}*'", "Nombre Departamento", "");
         }
     }
 }
